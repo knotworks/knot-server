6cfa39e598e79928f263965ade9ae8f3d1a526f6
syropian <collinpatrickhenderson@gmail.com>
2017-06-25 18:22:33 -0400

    Adds an Artisan command to quickly create invite codes for a given email

---
+++ app/Console/Commands/CreateInvite.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace FamJam\Console\Commands;
+
+use Illuminate\Console\Command;
+use Doorman;
+
+class CreateInvite extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'doorman:invite';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates an invite code for the specified email.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $email = $this->ask('What email address would you like to associate with this invite?');
+        Doorman::generate()->for($email)->make();
+        $this->info("Invite successfully generated for {$email}!");
+    }
+}
