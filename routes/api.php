234553c22bd700ba1f9e2b4d3a093bc52735be21
syropian <collinpatrickhenderson@gmail.com>
2017-08-16 13:07:34 -0400

    Small routes cleanup

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 Route::get('/auth/user', 'AuthController@user');
@@ -29,6 +24,7 @@ Route::delete('/posts/{post}', 'PostsController@destroy');
 Route::post('/posts/{post}/reactions', 'ReactionsController@store');
 Route::get('/posts/{post}/comments', 'CommentsController@index');
 Route::post('/posts/{post}/comments', 'CommentsController@store');
+
 Route::put('/comments/{comment}', 'CommentsController@update');
 Route::delete('/comments/{comment}', 'CommentsController@destroy');
 
