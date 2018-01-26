b75419fc3b53ccea06fc2ee2ffe27f08ccf577e3
Collin Henderson <hendo13@me.com>
2018-01-25 20:27:49 -0500

    update public index file

--- public/index.php
+++ public/index.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
- * Laravel - A PHP Framework For Web Artisans.
+ * Laravel - A PHP Framework For Web Artisans
  *
+ * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-
+define('LARAVEL_START', microtime(true));
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
@@ -16,10 +17,8 @@
 | into the script here so that we don't have to worry about manual
 | loading any of our classes later on. It feels great to relax.
 |
-*/
-
-require __DIR__.'/../bootstrap/autoload.php';
-
+ */
+require __DIR__ . '/../vendor/autoload.php';
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -30,10 +29,8 @@ require __DIR__.'/../bootstrap/autoload.php';
 | will load up this application so that we can run it and send
 | the responses back to the browser and delight our users.
 |
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
+ */
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 /*
 |--------------------------------------------------------------------------
 | Run The Application
@@ -44,14 +41,10 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | the client's browser allowing them to enjoy the creative
 | and wonderful application we have prepared for them.
 |
-*/
-
+ */
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
-
 $response->send();
-
 $kernel->terminate($request, $response);
