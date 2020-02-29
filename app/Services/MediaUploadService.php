e55496812fa03c8938db7fd17647b6de67437369
Collin Henderson <collinpatrickhenderson@gmail.com>
2020-02-29 10:00:59 -0500

    Simplify uploadImage function

--- app/Services/MediaUploadService.php
+++ app/Services/MediaUploadService.php
@@ -7,39 +7,21 @@ use JD\Cloudder\Facades\Cloudder;
 
 class MediaUploadService
 {
-    protected $image;
-    protected $name;
-
-    public function processImage($file): string
-    {
-        $path = 'uploads/media/images/'.$this->name.'.jpg';
-        $publicPath = public_path($path);
-
-        $this->image = Image::make($file)
-            ->orientate()
-            ->resize(config('image.max_width'), config('image.max_height'), function ($constraint) {
-                $constraint->aspectRatio();
-                $constraint->upsize();
-            })
-            ->save($publicPath, config('image.upload_quality'));
-
-        return $publicPath;
-    }
-
     public function uploadImage($file)
     {
-        $this->name = strtotime('now').'_'.pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $name = strtotime('now').'_'.pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $path = 'uploads/media/images/'.$name.'.jpg';
+        $publicPath = public_path($path);
+        $image = Image::make($file)->save($publicPath, config('image.upload_quality'));
 
-        $publicPath = $this->processImage($file);
-
-        $publicId = Cloudder::upload($publicPath, config('app.env').'/media/images/'.$this->name, ['angle' => 0])
+        $publicId = Cloudder::upload($publicPath, config('app.env').'/media/images/'.$name)
             ->getPublicId();
 
-        $imageWidth = $this->image->width();
-        $imageHeight = $this->image->height();
+        $imageWidth = $image->width();
+        $imageHeight = $image->height();
 
         // Destroy the image instance
-        $this->image->destroy();
+        $image->destroy();
         unlink($publicPath);
 
         return [
@@ -51,19 +33,19 @@ class MediaUploadService
 
     public function uploadVideo($file)
     {
-        $this->name = strtotime('now').'_'.pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $name = strtotime('now').'_'.pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
         $path = 'uploads/media/videos/';
         $publicPath = public_path($path);
-        $file->move($publicPath, $this->name.'.mp4');
+        $file->move($publicPath, $name.'.mp4');
 
-        $filePath = $publicPath.''.$this->name.'.mp4';
-        $upload = Cloudder::uploadVideo($filePath, config('app.env').'/media/videos/'.$this->name, ['start_offset' => 0, 'end_offset' => 30]);
+        $filePath = $publicPath.''.$name.'.mp4';
+        $upload = Cloudder::uploadVideo($filePath, config('app.env').'/media/videos/'.$name, ['start_offset' => 0, 'end_offset' => 30]);
         $result = $upload->getResult();
-        $publicId = $result['public_id'];
+
         unlink($filePath);
 
         return [
-            'publicId' => $publicId,
+            'publicId' => $result['public_id'],
             'width' => $result['width'],
             'height' => $result['height'],
         ];
